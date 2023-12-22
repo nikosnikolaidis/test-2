8a3ed139a99077c4ef6a59fa03cf68a606f49b20
Nikos Nikolaidis <nikosnikolaidis@users.noreply.github.com>
2023-12-22 18:01:21 +0000

    delete test

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
 
-
-    //test
    
     @GetMapping("/hello")
     public String hello(){
