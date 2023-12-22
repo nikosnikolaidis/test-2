d34325dc52e5d9b910c823ecac5fd85f63876d86
Nikos Nikolaidis <nikosnikolaidis@users.noreply.github.com>
2023-12-22 17:56:06 +0000

    init commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+   
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello!";
+    }
+}
